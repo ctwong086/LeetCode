7785d590103ffa1dfb35a55b85f3f27cff2aae2f
Marco Wong <wong332008@gmail.com>
2022-08-27 02:40:20 +0800

    Code Update

--- Medium/2. Add Two Numbers.js
+++ Medium/2. Add Two Numbers.js
@@ -24,3 +24,29 @@ let addTwoNumbers = function(l1, l2) {
     }
     return res.next;
 }
+
+//Attempt#2
+let addTwoNumbers = function(l1, l2) {
+    let res = new ListNode(0);
+    let cur = res;
+    let carry = 0;
+    
+    while(l1 !== null || l2 !== null) {
+        let sum = carry;
+        
+        if(l1 !== null) {
+            sum += l1.val;
+            l1 = l1.next;
+        };
+        if(l2 !== null) {
+            sum += l2.val;
+            l2 = l2.next;  
+        };
+        carry = sum > 9 ? 1 : 0;
+        sum = sum % 10;
+        cur.next = new ListNode(sum);
+        cur = cur.next;
+    }
+    if(carry === 1) {cur.next = new ListNode(carry)};
+    return res.next;
+}
